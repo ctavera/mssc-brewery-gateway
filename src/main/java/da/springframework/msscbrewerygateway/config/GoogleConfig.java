05e1997095c76d0782d8dd467a175de02602fe72
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-06-16 17:42:51 -0500

    255. Spring Cloud Gateway Route Configuration. Closes #1

---
+++ src/main/java/da/springframework/msscbrewerygateway/config/GoogleConfig.java
@@ -0,0 +1,21 @@
+package da.springframework.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/googlesearch2")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com"))
+                .build();
+    }
+}
