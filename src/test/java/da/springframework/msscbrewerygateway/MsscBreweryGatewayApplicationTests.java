aaf72c0f55d240e9be1b1fb19d7ce5ac5988de10
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-06-16 12:56:16 -0500

    254. Spring Cloud Gateway Service Creation. Initial commit.

---
+++ src/test/java/da/springframework/msscbrewerygateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.msscbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
