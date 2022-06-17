00eb99bac262df4b1bc938d74298f5840aa75432
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-06-17 17:07:56 -0500

    257. Beer Service Route Configuration. Closes #2

---
+++ src/main/java/da/springframework/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,18 @@
+package da.springframework.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(predicateSpec -> predicateSpec.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080"))
+                .build();
+    }
+}
